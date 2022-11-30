5888900b250bd9ef2ca1cedb48265f2d6cfba0ab
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-30 20:20:08 +0530

    Added:logic for Display the welcome message of gambling simulator game

---
+++ src/com/java/gamblinggame/GamblingSimulator.java
@@ -0,0 +1,10 @@
+package com.java.gamblinggame;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the gambling simulator program");
+
+	}
+
+}

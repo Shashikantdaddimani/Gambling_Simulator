6937661482c26ba6ddc991616f8f289d8dac80ce
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-30 20:42:33 +0530

    Added:Uc-1 As a Gambler, Would start with a stake of 00 every day and bet  every game

--- src/com/java/gamblinggame/GamblingSimulator.java
+++ src/com/java/gamblinggame/GamblingSimulator.java
@@ -1,10 +1,21 @@
 package com.java.gamblinggame;
 
 public class GamblingSimulator {
+	/*
+	 * Declaring and initializing Variable.
+	 */
+	public static final int initialStake = 100;
+	public static final int betStake = 1;
 
+	/**
+	 * program execution starts from main method
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the gambling simulator program");
-
+		System.out.println("Initial Stake is : " + initialStake + "$");
+		System.out.println("Bet Stake is :" + betStake + "$");
 	}
 
 }

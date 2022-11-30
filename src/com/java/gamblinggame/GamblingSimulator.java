dda8d85a486a9ef61966bc438c316db0cd3f2bf3
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-30 21:08:03 +0530

    Uc2-Added:logic for checking winning or loossing gambler game

--- src/com/java/gamblinggame/GamblingSimulator.java
+++ src/com/java/gamblinggame/GamblingSimulator.java
@@ -1,4 +1,8 @@
 package com.java.gamblinggame;
+/**
+ * imports all the class of the java.util package
+ */
+import java.util.Random;
 
 public class GamblingSimulator {
 	/*
@@ -6,16 +10,34 @@ public class GamblingSimulator {
 	 */
 	public static final int initialStake = 100;
 	public static final int betStake = 1;
+	/*
+	 * Taking random Object by generating random number
+	 */
+	static Random random = new Random();
 
 	/**
-	 * program execution starts from main method
+	 * PSrogram execution starts from main method
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int totalStake = initialStake;
 		System.out.println("Welcome to the gambling simulator program");
-		System.out.println("Initial Stake is : " + initialStake + "$");
-		System.out.println("Bet Stake is :" + betStake + "$");
+		int play = random.nextInt(2);
+		/*
+		 * logic for checking winning or loosing by using switch case
+		 */
+		switch (play) {
+		case 0:
+			totalStake = totalStake - betStake;
+			System.out.println("Stake After loosing :" + totalStake);
+			break;
+		case 1:
+			totalStake = totalStake + betStake;
+			System.out.println("Stake After Winning :" + totalStake);
+			break;
+
+		}
 	}
 
 }

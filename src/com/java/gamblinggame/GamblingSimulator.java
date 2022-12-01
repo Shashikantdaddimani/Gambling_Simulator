eed6244df838c9cb425d50936da1a4085d652995
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-01 11:28:14 +0530

    Uc4-Added:Logic for checking Player won or lost after 20days of playing every day

--- src/com/java/gamblinggame/GamblingSimulator.java
+++ src/com/java/gamblinggame/GamblingSimulator.java
@@ -14,6 +14,10 @@ public class GamblingSimulator {
 	public static final float percentage50 = (initialStake / 100) * 50;
 	public static final float highestStake = percentage50 + initialStake;
 	public static final float lowestStake = percentage50 - initialStake;
+	public static final int START_DAY = 1;
+	public static final int MAX_DAY = 20;
+	public static final int STAKES_PER_DAY = 50;
+	public static final int START_DOLLARS = 0;
 	/*
 	 * Taking random Object by generating random number
 	 */
@@ -25,32 +29,35 @@ public class GamblingSimulator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int totalStake = initialStake;
-		System.out.println("Welcome to the gambling simulator program");
-		while (totalStake < highestStake && totalStake > lowestStake) {
-			int play = random.nextInt(2);
-			/*
-			 * logic for checking winning or loosing by using switch case
-			 */
-			switch (play) {
-			case 0:
-				totalStake = totalStake - betStake;
-				System.out.println("Stake After loosing :" + totalStake);
-				break;
-			case 1:
-				totalStake = totalStake + betStake;
-				System.out.println("Stake After Winning :" + totalStake);
-				break;
 
+		System.out.println("Welcome to the gambling simulator program");
+		int days = START_DAY;
+		int totalDollars = START_DOLLARS;
+		
+		for (days = START_DAY; days <= MAX_DAY; days++) {
+			int totalStake = initialStake;
+
+			while (totalStake < highestStake && totalStake > lowestStake) {
+				int luck = (int) (Math.random() * 10) % 2;
+				if (luck == 1) {
+					totalStake = totalStake + betStake;
+				} else {
+					totalStake = totalStake - betStake;
+				}
+			}
+			if (totalStake == highestStake) {
+				totalDollars += STAKES_PER_DAY;
+				System.out.println("Gambler won by " + totalDollars + " dollars on day " + days);
+			} else {
+				totalDollars -= STAKES_PER_DAY;
+				System.out.println("Gambler lost by " + totalDollars + " dollars on day " + days);
 			}
 		}
-		/*
-		 * Logic for checking Won or lost
-		 */
-		if (totalStake == highestStake)
-			System.out.println("Gambler won by: " + totalStake);
-		else
-			System.out.println("Gambler lost by: " + totalStake);
+		if (totalDollars > START_DOLLARS) {
+			System.out.println("Gambler won " + totalDollars);
+		} else {
+			System.out.println("Gambler lost " + totalDollars);
+		}
 
 	}
 }
